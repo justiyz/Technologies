26bca4be1941043d3c96e982775f744917b84217
Ernest <inyange8888@gmail.com>
2023-07-14 11:10:19 +0100

    thread

--- src/main/java/com/technologies/thread/MultiThreadingExtendsThread.java
+++ src/main/java/com/technologies/thread/MultiThreadingExtendsThread.java
@@ -2,13 +2,19 @@ package com.technologies.thread;
 
 //There are two ways to achieve multi-threading
 //The first way is to extend the Thread class and override the run method which is done below
-public class MultiThreading extends Thread{
+public class MultiThreadingExtendsThread extends Thread{
+
+    private final int threadNumber;
+
+    public MultiThreadingExtendsThread(int threadNumber){
+        this.threadNumber = threadNumber;
+    }
 
     @Override
     public void run() {
         //write whatever method or code that you want to run in multiple threads inside this overridden run method
-        for (int i = 1; i <= 5; i++){
-            System.out.println(i);
+        for (int i = 1; i <= 3; i++){
+            System.out.println(i + " from thread " + threadNumber);
 
             //sleep for one second(1000 milliseconds) between each iteration
             try {
--- src/main/java/com/technologies/thread/MultiThreadingImpl.java
+++ src/main/java/com/technologies/thread/MultiThreadingImpl.java
@@ -5,7 +5,7 @@ public class MultiThreadingImpl {
     public static void main(String[] args) {
 
         for (int i = 1; i < 5; i++) {
-            MultiThreading threading = new MultiThreading();
+            MultiThreadingExtendsThread threading = new MultiThreadingExtendsThread(i);
             threading.start();
         }
 
