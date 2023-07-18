55aba6bfa1bff39d514ff545e0cac70330a7cb6e
Ernest <inyange8888@gmail.com>
2023-07-18 18:38:34 +0100

    annotations

--- src/main/java/com/technologies/annotations/Cat.java
+++ src/main/java/com/technologies/annotations/Cat.java
@@ -5,10 +5,6 @@ public class Cat {
     String name;
     int age;
 
-    public Cat(String name, int age){
-        this.name = name;
-        this.age = age;
-    }
     public Cat(String name){
         this.name = name;
     }
