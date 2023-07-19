02a7fb64768d7f6b52fe939e347b04a75b2bc84f
Ernest <inyange8888@gmail.com>
2023-07-19 19:18:41 +0100

    custom annotations

--- src/main/java/com/technologies/annotations/CreatingCustomAnnotationsImpl.java
+++ src/main/java/com/technologies/annotations/CreatingCustomAnnotationsImpl.java
@@ -1,7 +1,20 @@
 package com.technologies.annotations;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class CreatingCustomAnnotationsImpl {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         Cat cat = new Cat("Billy");
+
+        //THIS CHECKS FOR THE PRESENCE OF AN ANNOTATION
+        if (cat.getClass().isAnnotationPresent(VeryImportant.class)){
+            System.out.println("Very important is present");
+        } else {
+            System.out.println("Very important is not present");
+        }
+
+
+
     }
 }
