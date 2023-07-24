6698a1cd71e15b573c6da7411ae0858bb07e61f5
Ernest <inyange8888@gmail.com>
2023-07-24 11:43:27 +0100

    unchecked exceptions

--- src/main/java/com/technologies/exceptions/checkedVSunchecked/Checked.java
+++ src/main/java/com/technologies/exceptions/checkedVSunchecked/Checked.java
@@ -3,14 +3,15 @@ package com.technologies.exceptions.checkedVSunchecked;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-public class CheckedVsUnchecked {
+public class Checked {
     public static void main(String[] args) {
 
         readfile("myFile.txt");
     }
 
+    //A CHECKED EXCEPTION ARE EXCEPTIONS THAT JAVA MAKES YOU DEAL WITH AT COMPILE TIME.
+
     private static void readfile(String fileName){
-        //A CHECKED EXCEPTION ARE EXCEPTIONS THAT JAVA MAKES YOU DEAL WITH AT COMPILE TIME.
         //TO HANDLE AN UNCHECKED EXCEPTION, YOU EITHER USE A TRY-CATCH BLOCK OR YOU SURROUND THE METHOD WITH A "THROWS" STATEMENT
         try {
             FileReader reader = new FileReader(fileName);
---
+++ src/main/java/com/technologies/exceptions/checkedVSunchecked/Unchecked.java
@@ -0,0 +1,17 @@
+package com.technologies.exceptions.checkedVSunchecked;
+
+
+public class Unchecked {
+
+    public static void main(String[] args) {
+
+        printLength("myFile.txt");
+    }
+
+    //AN UNCHECKED EXCEPTION ARE EXCEPTIONS THAT JAVA MAKES YOU DEAL WITH AT COMPILE TIME.
+
+    private static void printLength(String s){
+        //TO HANDLE AN UNCHECKED EXCEPTION, YOU EITHER USE A TRY-CATCH BLOCK OR YOU DECLARE THAT YOU THROW IT WITH A "THROWS" STATEMENT
+
+    }
+}
